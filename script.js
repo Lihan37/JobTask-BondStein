90316d9d1e1f18d89d212d6eb4bd56337514fece
Eanur Rahman <lihan.lihu@gmail.com>
2023-12-26 05:08:53 +0600

    I have completed the task and added some extra info

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function toggleMobileMenu() {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.style.display = (mobileMenu.style.display === 'block') ? 'none' : 'block';
+document.addEventListener('DOMContentLoaded', function () {
+  const initialRoute = window.location.hash.slice(1);
+  showPage(initialRoute);
+
+  window.addEventListener('hashchange', function () {
+      const route = window.location.hash.slice(1);
+      showPage(route);
+  });
+});
+
+function showPage(route) {
+  const pageSections = document.querySelectorAll('.page-section');
+  pageSections.forEach(section => {
+      section.style.display = 'none';
+  });
+
+  const selectedSection = document.getElementById(route);
+  if (selectedSection) {
+      selectedSection.style.display = 'block';
+      toggleMobileMenu();
   }
-  
+}
+
+function toggleMobileMenu() {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.style.display = (mobileMenu.style.display === 'block') ? 'none' : 'block';
+}
