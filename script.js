5429d94f4805a3fc38326f036f9b378ac2f16ff6
Eanur Rahman <lihan.lihu@gmail.com>
2023-12-26 04:20:36 +0600

    I have added css to the navbar and made it responsive

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMobileMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.style.display = (mobileMenu.style.display === 'block') ? 'none' : 'block';
+  }
+  
